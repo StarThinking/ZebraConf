c3bb3e46263a5de326a1fe7a99a70194c6aa5003
root <root@node-0.16-g5.postman-pg0.wisc.cloudlab.us>
2021-01-27 15:56:19 -0600

    a small fix

--- runner/sbin/cluster/rebuild_cluster_containers.sh
+++ runner/sbin/cluster/rebuild_cluster_containers.sh
@@ -3,7 +3,7 @@
 if [ $# -ne 2 ]; then echo "wrong [tag] [vms]"; exit -1; fi
 pm=( $(grep -oP "node-[0-9]{1,2}$" /etc/hosts | sed 's/node-//g' | sort -n) )
 tag=$1
-vms=$(( $2 - 1 ))
+vms=$2
 
 pids=()
 
