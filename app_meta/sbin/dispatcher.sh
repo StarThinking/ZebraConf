1e66ff3f54ed67123f968c8f004e784c7ef782a5
root <ma.1189@osu.edu>
2021-01-30 22:04:04 +0000

    modify pre-run dispatcher

--- app_meta/sbin/dispatcher.sh
+++ app_meta/sbin/dispatcher.sh
@@ -3,24 +3,18 @@
 if [ $# -ne 1 ]; then echo 'ERROR: ./dispatcher.sh [project]'; exit -1; fi
 
 the_project=$1
-vm_num=$(( $(cat /proc/cpuinfo | grep 'processor' | wc -l) / 2 ))
-if [ $vm_num -gt 20 ]; then vm_num=20; fi
-vm_num=10
-vm_num=$(( vm_num -1 ))
 busy_file=/tmp/reconf_busy
-dst='/root/reconf_test_gen/target/'
 IFS=$'\n' 
-entry_list=( $(cat /root/reconf_test_gen/task.txt) )
+entry_list=( $(cat /root/ZebraConf/app_meta/task.txt) )
 entry_list_length=${#entry_list[@]}
 entry_cursor=0
-echo entry_list_length = $entry_list_length
-cmd='echo /root/reconf_test_gen/run_mvn_test.sh $the_project ${entry_list[$entry_cursor]} $dst'
+echo "task size = $entry_list_length"
+cmd='echo /root/ZebraConf/app_meta/run_mvn_test.sh $the_project ${entry_list[$entry_cursor]}'
 
 function is_busy {
-    i=$1
-    jps_num=$(docker exec hadoop-$i bash -c "jps" | wc -l)
-    #sh_num=$(docker exec hadoop-$i bash -c "ps aux | grep run_mvn_test.sh" | wc -l)
-    if [ $jps_num -gt 1 ] ; then
+    my_con=$1
+    jps_num=$(docker exec $my_con bash -c "jps" | wc -l)
+    if [ $jps_num -gt 1 ]; then
 	echo "true"
     else
 	echo "false"
@@ -29,13 +23,19 @@ function is_busy {
 
 while [ $entry_cursor -lt $entry_list_length ]
 do
-    for i in $(seq 0 $vm_num)
+    for my_container in $(docker container list -a | awk '{print $NF}' | grep -v NAMES)
     do
-        if [ "$(is_busy $i)" == "true" ]; then
-	     echo hadoop-$i is busy
+        if [ "$(is_busy $my_container)" == "true" ]; then
+	     echo "$my_container is busy"
 	else
-	    echo hadoop-$i is not busy, assign entry $entry_cursor to it
-	    docker exec -d hadoop-$i bash -c "$(eval $cmd)"
+            # double check
+            for dc in 1 2 3 4 5; do
+                if [ "$(is_busy $my_container)" == "true" ]; then
+                    continue
+                fi
+            done
+	    echo "$my_container is not busy, assign entry $entry_cursor to it"
+	    docker exec -d $my_container bash -c "$(eval $cmd)"
 	    entry_cursor=$(( entry_cursor + 1 ))
 	    if [ $entry_cursor -ge $entry_list_length ]; then echo finish all tasks; break; fi
 	fi
--- runner/sbin/local/clean_and_build.sh
+++ runner/sbin/local/clean_and_build.sh
@@ -6,9 +6,9 @@ killall -9 dispatcher.sh
 pkill dispatcher.sh
 
 pids=()
-for con in $(docker container list -a | awk '{print $NF}' | grep -v NAMES)
+for my_container in $(docker container list -a | awk '{print $NF}' | grep -v NAMES)
 do 
-    docker exec $con bash -c "pkill -9 java; cd /root/ZebraConf; git clean -df; git checkout -- *; git pull; cd runner; ./build.sh" &
+    docker exec $my_container bash -c "pkill -9 java; cd /root/ZebraConf; git clean -df; git checkout -- *; git pull; cd runner; ./build.sh" &
     pids[$i]=$!
 done
 
