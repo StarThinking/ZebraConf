8178576f944f1253f3474ac51be70799e4815923
root <root@node-0.my20g5.postman-pg0.wisc.cloudlab.us>
2021-02-04 22:21:09 -0600

    simple fix

--- runner/sbin/update_cluster_white_list.sh
+++ runner/sbin/update_cluster_white_list.sh
@@ -10,10 +10,10 @@ do
     scp $new_file node-$i:/root/vm_images/ZebraConf/runner/
 done
 
-# second, run cp_from_local_2_cont.sh asynchronized
+# second, run cp_from_local_2_cont.sh asynchronizedly
 for i in $(grep -oP "node-[0-9]{1,2}$" /etc/hosts | sed 's/node-//g' | sort -n)
 do 
-    ssh node-$i "/root/vm_images/ZebraConf/container_sbin/local/cp_from_local_2_cont.sh /root/vm_images/ZebraConf/runner/ /root/ZebraConf/runner/white_list.txt &" & pids+=($!)
+    ssh node-$i "/root/vm_images/ZebraConf/container_sbin/local/cp_from_local_2_cont.sh /root/vm_images/ZebraConf/runner/white_list.txt /root/ZebraConf/runner/white_list.txt &" & pids+=($!)
 done
 
 for p in ${pids[@]}
