eb8dad512455e8489e864e1b07dde517e54d2199
StarThinking <ma.1189@osu.edu>
2021-02-22 21:58:45 +0000

    update

--- app_meta/run_mvn_test.sh
+++ app_meta/run_mvn_test.sh
@@ -41,10 +41,11 @@ LOG_TIME="$(($(date +%s%N)/1000000))"
 if [ ! -d $log_dts_dir ]; then 
     mkdir $log_dts_dir
 fi
-mv $output_log $log_dts_dir/"$the_test"-output.txt
+my_random_name="$the_test"-output_"$LOG_TIME"_"$RANDOM$RANDOM".txt
+mv $output_log $log_dts_dir/$my_random_name
 # append output_log path and return code to our log
-echo "msx-output-log $output_log" >> $log_dts_dir/"$the_test"-output_"$LOG_TIME"_"$RANDOM$RANDOM".txt
-echo "msx-rc $rc" >> $log_dts_dir/"$the_test"-output_"$LOG_TIME"_"$RANDOM$RANDOM".txt
+echo "msx-output-log $output_log" >> $log_dts_dir/$my_random_name
+echo "msx-rc $rc" >> $log_dts_dir/$my_random_name
 
 # return exit code of mvn test
 exit $rc
--- runner/sbin/check_thread_consistency.sh
+++ runner/sbin/check_thread_consistency.sh
@@ -2,8 +2,7 @@
 
 if [ $# -ne 1 ]; then echo './check_thread_consistency.sh [log]'; exit -1; fi
 log=$1
-TMP_OUTPUT=~/my_check_thread_consistency.tmp.txt
+TMP_OUTPUT=/root/my_check_thread_consistency.tmp.txt
 rm $TMP_OUTPUT &> /dev/null
 cat $log | grep 'get-return' | grep -v 'thread is 1'$ | sort -u | awk '{print $NF" "$4" "$5}' | sort -u > $TMP_OUTPUT
-cat ~/my_check_thread_consistency.tmp | awk '{print $1}' | sort -u | while read tid; do if [ $(grep "$tid " $TMP_OUTPUT | wc -l) -gt 1 ]; then echo "warning! thread $tid has multiple value"; fi; done
-rm $TMP_OUTPUT &> /dev/null
+cat $TMP_OUTPUT | awk '{print $1}' | sort -u | while read tid; do if [ $(grep "$tid " $TMP_OUTPUT | wc -l) -gt 1 ]; then echo "warning! thread $tid has multiple value"; fi; done
