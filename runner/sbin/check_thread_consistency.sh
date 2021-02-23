b286a9467ffe1ac3c6ef62e524c43d864748d887
root <root@node-0.my20g5.postman-pg0.wisc.cloudlab.us>
2021-02-22 18:43:02 -0600

    modify check_thread_consistency.sh

--- runner/sbin/check_thread_consistency.sh
+++ runner/sbin/check_thread_consistency.sh
@@ -4,5 +4,6 @@ if [ $# -ne 1 ]; then echo './check_thread_consistency.sh [log]'; exit -1; fi
 log=$1
 TMP_OUTPUT=/root/my_check_thread_consistency.tmp.txt
 rm $TMP_OUTPUT &> /dev/null
-cat $log | grep 'get-return' | grep -v 'thread is 1'$ | sort -u | awk '{print $NF" "$4" "$5}' | sort -u > $TMP_OUTPUT
-cat $TMP_OUTPUT | awk '{print $1}' | sort -u | while read tid; do if [ $(grep "$tid " $TMP_OUTPUT | wc -l) -gt 1 ]; then echo "warning! thread $tid has multiple value"; fi; done
+cat $log | grep 'msx-confcontroller get-return' | sort -u | awk '{print $NF" "$(NF-3)" "$4" "$5}' | sort -u > $TMP_OUTPUT
+cat $TMP_OUTPUT
+cat $TMP_OUTPUT | awk -F ',' '{print $1}' | sort -u | while read tid; do if [ $(grep "$tid " $TMP_OUTPUT | wc -l) -gt 1 ]; then echo "warning! thread $tid has multiple value"; fi; done
