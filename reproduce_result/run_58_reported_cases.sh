d484552a555bf270183d358a6a17c3ccca1db44c
StarThinking <ma.1189@osu.edu>
2021-02-24 02:40:04 +0000

    modify file path

--- reproduce_result/run_58_reported_cases.sh
+++ reproduce_result/run_58_reported_cases.sh
@@ -4,4 +4,4 @@ while IFS=' ' read -r a1 a2 a3
 do	
 	#echo $a1 $a2 $a3
 	/root/ZebraConf/runner/run_heter_conf_test.sh $a1 $a2 $a3
-done < 58_reported_cases.txt
+done < /root/ZebraConf/reproduce_result/58_reported_cases.txt
