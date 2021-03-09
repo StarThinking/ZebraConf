7a9ffd0c8212b0b4dbeef198e38f61e3cdadcdf4
StarThinking <ma.1189@osu.edu>
2021-03-09 03:31:45 +0000

    remove blank line by this change in sbin/compress.sh

--- app_meta/sbin/compress.sh
+++ app_meta/sbin/compress.sh
@@ -6,8 +6,8 @@ log=$1
 #if [ "$(cat $log | grep "msx-reconfagent" | grep "performReconf")" == "" ]; then echo 'no init'; exit 0; fi
 
 test_name=$(echo $log | awk -F '-output.txt' '{print $1}')
-grep msx $log | grep -v 'msx-get' | sort -u > "$test_name"-component-meta.txt
+grep msx $log | grep -v 'msx-get' | sort -u | sed -r '/^\s*$/d' > "$test_name"-component-meta.txt
 
-grep msx $log | grep 'msx-get' | awk -F 'msx-get ' '{print $2}' | sort -u > "$test_name"-parameter-meta.txt
+grep msx $log | grep 'msx-get' | awk -F 'msx-get ' '{print $2}' | sort -u | sed -r '/^\s*$/d' > "$test_name"-parameter-meta.txt
 
-cat "$test_name"-parameter-meta.txt | awk '{print $1" "$2" "$3}' | sort -u > "$test_name"-ultimate-meta.txt
+cat "$test_name"-parameter-meta.txt | awk '{print $1" "$2" "$3}' | sort -u | sed -r '/^\s*$/d' > "$test_name"-ultimate-meta.txt
