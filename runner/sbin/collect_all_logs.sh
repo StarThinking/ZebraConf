44fd62bbcc1132357822ac89fe053c19f0415517
root <root@node-0.9g1.postman-pg0.wisc.cloudlab.us>
2021-03-24 15:32:06 -0500

    update

--- runner/sbin/collect_all_logs.sh
+++ runner/sbin/collect_all_logs.sh
@@ -2,6 +2,9 @@
 
 if [ $# -ne 1 ]; then echo '[new_dir]'; exit -1; fi
 
+# kill auto_update_white_list.sh before collect all
+ps aux | grep auto_update_white_list.sh | awk '{print $2}' | xargs kill -9
+
 ~/vm_images/ZebraConf/runner/sbin/collect_combine_run.sh
 rm mv ~/vm_images/ZebraConf/runner/log/*txt
 mv ~/vm_images/ZebraConf/runner/log ~/vm_images/ZebraConf/runner/log_combine
