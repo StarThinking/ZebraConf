3c4538e88c74e1be78cd58ee953affc1bac66ee0
root <root@node-0.7-g1.postman-pg0.wisc.cloudlab.us>
2021-03-24 15:00:40 -0500

    add sbin/collect_all_logs.sh

---
+++ runner/sbin/collect_all_logs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then echo '[new_dir]'; fi
+
+~/vm_images/ZebraConf/runner/sbin/collect_combine_run.sh
+rm mv ~/vm_images/ZebraConf/runner/log/*txt
+mv ~/vm_images/ZebraConf/runner/log ~/vm_images/ZebraConf/runner/log_combine
+
+~/vm_images/ZebraConf/runner/sbin/collect_hypothesis.sh
+rm mv ~/vm_images/ZebraConf/runner/log/*txt
+mv ~/vm_images/ZebraConf/runner/log ~/vm_images/ZebraConf/runner/log_hypothesis
+
+~/vm_images/ZebraConf/runner/sbin/collect_single_run.sh
+rm mv ~/vm_images/ZebraConf/runner/log/*txt
+mv ~/vm_images/ZebraConf/runner/log ~/vm_images/ZebraConf/runner/log_collect_combine
+
+mkdir ~/vm_images/ZebraConf/runner/"$1"
+mv ~/vm_images/ZebraConf/runner/log_* ~/vm_images/ZebraConf/runner/"$1" 
+
+
