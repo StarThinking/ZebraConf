522265221069329e9feea1f5b92e5c3af88ac297
StarThinking <ma.1189@osu.edu>
2022-01-24 22:47:15 -0600

    update

--- app_meta/run_all_cassandra_pytest.sh
+++ app_meta/run_all_cassandra_pytest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for test in $(cat cassandra/test_suite/all_test_func_level.txt); do echo $test; time ./run_pytest.sh cassandra $test false true &> tmp_pytest_logs/$test; done
+for test in $(cat $1); do echo $test; time ./run_pytest.sh cassandra $test false true &> tmp_pytest_logs/$test; done
