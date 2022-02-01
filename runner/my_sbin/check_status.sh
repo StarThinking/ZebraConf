afc86ac5a2aaf55d543ebd286e5f00f0e16d7ce0
root <root@node0.newhead.postman-pg0.wisc.cloudlab.us>
2022-02-01 11:21:29 -0600

    update

--- runner/my_sbin/check_status.sh
+++ runner/my_sbin/check_status.sh
@@ -4,7 +4,7 @@ for i in $(seq 0 4)
 do 
     last="$(docker exec container-$i bash -c 'cat /root/vm_images/ZebraConf/runner/nohup.txt' | grep --text 'assign entry' | tail -n 1 | awk '{print $(NF - 2)}')"
     task_num=$(docker exec container-$i bash -c 'cat /root/vm_images/ZebraConf/runner/task.txt | wc -l')
-    echo "container-$i: $last / $task_num"
+    echo "container-$i: $(( $last * 2 )) / $(( $task_num * 2 ))"
     docker exec container-$i bash -c 'find /root/vm_images/ZebraConf/runner/log -name *single_hypo* | while read i; do cat $i | head -n 4 | tail -n 1; cat $i | tail -n 4 | head -n 2; done'
     echo ''
 done
